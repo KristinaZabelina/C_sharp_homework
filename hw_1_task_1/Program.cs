e9b9e6c107b24e99693c70cce864d98cb7119427
Kristina <zkvzkvzkv@yandex.ru>
2023-01-21 18:28:02 +0300

    написан код задачи 1

--- hw_1_task_1/Program.cs
+++ hw_1_task_1/Program.cs
@@ -2,3 +2,19 @@
 // которая на вход принимает два числа и выдаёт, 
 // какое число большее, а какое меньшее.
 
+Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int min = b;
+if (b > max)
+{
+  max = b;
+  min = a;
+}
+
+Console.Write(max);
+Console.WriteLine(" - большее");
+Console.Write(min);
+Console.WriteLine(" - меньшее");
